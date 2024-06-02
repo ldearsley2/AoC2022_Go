31982d38225f8ea69c29f2453e54ec5e98c3bf46
ldearsley2 <ldearsley2@gmail.com>
2024-06-02 21:48:24 +0100

    Fix

--- day1/main.go
+++ day1/main.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
-	file, err := os.Open("day1/test_input.txt")
+	file, err := os.Open("day1/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -17,11 +18,24 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	var tempCalories, mostCalories = 0, 0
+
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		var calories, err = strconv.Atoi(scanner.Text())
+
+		if err != nil {
+			if tempCalories > mostCalories {
+				mostCalories = tempCalories
+			}
+			tempCalories = 0
+		}
+
+		tempCalories += calories
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Print(mostCalories)
 }
