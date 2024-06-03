e745939dcb01521d1160f710105974b8bc88afb9
ldearsley2 <ldearsley2@gmail.com>
2024-06-03 13:41:26 +0100

    Part 2

--- day1/main.go
+++ day1/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -37,5 +38,42 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Print(mostCalories)
+	fmt.Println(mostCalories)
+	fmt.Println(part2("day1/input.txt"))
+}
+
+func part2(filename string) int {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	var tempCalories int = 0
+	caloriesList := make([]int, 0)
+
+	for scanner.Scan() {
+		var calories, err = strconv.Atoi(scanner.Text())
+		if err != nil {
+			caloriesList = append(caloriesList, tempCalories)
+			tempCalories = 0
+		}
+		tempCalories += calories
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	var total int = 0
+	sort.Slice(caloriesList, func(i, j int) bool {
+		return caloriesList[i] < caloriesList[j]
+	})
+
+	for i := range 3 {
+		total += caloriesList[len(caloriesList)-1-i]
+	}
+
+	return total
 }
