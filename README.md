22457f8f716b6fb8d493379f0368c236c64f4864
ldearsley2 <ldearsley2@gmail.com>
2024-06-05 18:52:20 +0100

    day2 part 2, readme update

---
+++ README.md
@@ -0,0 +1,10 @@
+# Advent of Code 2022 (GoLang)
+
+Link: https://adventofcode.com/2022
+### Day 1
+Learning how to open and read a file with Golang by utilising a scanner.
+
+### Day 2
+Utilising a hashmap to define the scores for each round.
+
+### Day 3
--- day2/main.go
+++ day2/main.go
@@ -36,4 +36,37 @@ func main() {
 	}
 
 	fmt.Println(total)
+	fmt.Println(part2())
+}
+
+func part2() int {
+	// x, y, z = lose, draw, win
+
+	var matchScores = map[string]int{
+		"A X": 3,
+		"B X": 1,
+		"C X": 2,
+		"A Y": 4,
+		"B Y": 5,
+		"C Y": 6,
+		"A Z": 8,
+		"B Z": 9,
+		"C Z": 7,
+	}
+
+	file, err := os.Open("day2/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	total := 0
+
+	for scanner.Scan() {
+		total += matchScores[scanner.Text()]
+	}
+
+	return total
 }
