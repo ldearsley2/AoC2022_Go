2431b6b1043e160cbaa55058208af10dd44da9cd
ldearsley2 <ldearsley2@gmail.com>
2024-06-08 12:25:23 +0100

    day3 part1

--- day3/main.go
+++ day3/main.go
@@ -2,41 +2,46 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"strings"
+	"unicode"
 )
 
 //Each half of string is one of the compartments
 
 func main() {
-	//var letterPriority = map[string]int{
-	//	"a": 1,
-	//	"b": 2,
-	//	"c": 3,
-	//	"d": 4,
-	//	"e": 5,
-	//	"f": 6,
-	//	"g": 7,
-	//	"h": 8,
-	//	"i": 9,
-	//	"j": 10,
-	//	"k": 11,
-	//	"l": 12,
-	//	"m": 13,
-	//	"n": 14,
-	//	"o": 15,
-	//	"p": 16,
-	//	"q": 17,
-	//	"r": 18,
-	//	"s": 19,
-	//	"t": 20,
-	//	"w": 21,
-	//	"x": 22,
-	//	"y": 23,
-	//	"z": 24,
-	//}
+	var letterPriority = map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+		"d": 4,
+		"e": 5,
+		"f": 6,
+		"g": 7,
+		"h": 8,
+		"i": 9,
+		"j": 10,
+		"k": 11,
+		"l": 12,
+		"m": 13,
+		"n": 14,
+		"o": 15,
+		"p": 16,
+		"q": 17,
+		"r": 18,
+		"s": 19,
+		"t": 20,
+		"u": 21,
+		"v": 22,
+		"w": 23,
+		"x": 24,
+		"y": 25,
+		"z": 26,
+	}
 
-	file, err := os.Open("day3/test_input.txt")
+	file, err := os.Open("day3/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -44,12 +49,24 @@ func main() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
+	total := 0
+
 	for scanner.Scan() {
 		first := scanner.Text()[:len(scanner.Text())/2]
 		second := scanner.Text()[len(scanner.Text())/2:]
 
-		for i := range len(first) {
+		for _, c := range first {
+			if strings.Contains(second, string(c)) {
+				fmt.Printf("%c found in second compartment\n", c)
+				if unicode.IsUpper(c) {
+					total += 26 + letterPriority[strings.ToLower(string(c))]
+					break
+				}
+				total += letterPriority[string(c)]
+				break
+			}
 		}
 	}
 
+	fmt.Println(total)
 }
